565e880d21d0267cb06065f1e0b65a7daa2a9bd5
学生李令新 <stullx@qq.com>
2018-03-03 18:32:32 +0800

    判断是否是子树

---
+++ src/main/SwordOffer/SubTree.java
@@ -0,0 +1,45 @@
+/**
+ * @author: 李令新 stullx@qq.com
+ * @date: 2018.3.2
+ * @decription: 输入两棵二叉树A，B，判断B是不是A的子结构。 （ps：我们约定空树不是任意一个树的子结构）
+ */
+public class SubTree
+    {
+        public boolean HasSubtree(TreeNode root1, TreeNode root2)
+            {
+                boolean result = false;
+                if (root1 != null && root2 != null)
+                {
+                    if (root1.val == root2.val)
+                    {
+                        result = isSubtree(root1, root2);
+                    }
+                    if (!result)
+                    {
+                        result = HasSubtree(root1.left, root2);
+                    }
+                    if (!result)
+                    {
+                        result = HasSubtree(root1.right, root2);
+                    }
+                }
+                return result;
+            }
+        
+        public boolean isSubtree(TreeNode root1, TreeNode root2)
+            {
+                if (root2 == null)
+                {
+                    return true;
+                }
+                if (root1 == null)
+                {
+                    return false;
+                }
+                if (root1.val != root2.val)
+                {
+                    return false;
+                }
+                return isSubtree(root1.left, root2.left) && isSubtree(root1.right, root2.right);
+            }
+    }
